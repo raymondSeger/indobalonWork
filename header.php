2b5be5576fdcdf8478b4911552b4814305705c02
Raymond Seger <https://www.facebook.com/raymond.seger.37>
2014-10-20 14:31:17 +0700

    Commit 1 Saving Point
    Commit 1 Saving Point

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
+	<!-- Indobalon Favicon-->
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/asset/images/indobalon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/asset/images/indobalon.ico" type="image/x-icon" />
+<?php wp_head(); ?>
+</head>
+
---
+++ index.php
@@ -0,0 +1,164 @@
+<?php
+get_header();
+?>
+<body>
+
+<!-- navBar section -->
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container-fluid">
+
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navBarTarget">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/asset/images/main_logo.png" alt=""/>
+			</a>
+		</div>
+
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="navBarTarget">
+			<ul class="nav navbar-nav navbar-left">
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+
+	</div><!-- /.container-fluid -->
+</div>
+<!-- END of navBar section -->
+
+<!-- TopCool Section -->
+<!-- END of TopCool Section -->
+
+<!-- Balon section -->
+<!-- END of Balon -->
+
+
+<!-- How to order -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<h1 class="specialRedColor">HOW TO ORDER</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-xs-offset-1 col-xs-5 box-color-1">
+			<hr class="veryShortRedHR"/>
+			<h1 class="text-center text-uppercase specialBlackColor">Pilih Produk</h1>
+			<p>Silahkan anda memilih katalog produk berupa balon foil huruf & angka yang terdiri dari warna emas dan perak, balon hati yang terdiri dari 7 pilihan warna.</p>
+		</div>
+
+		<div class="col-xs-5 box-color-2">
+			<hr class="veryShortRedHR"/>
+			<h1 class="text-center text-uppercase specialBlackColor">Hubungi Kami</h1>
+			<p>Untuk pemesanan balon, anda dapat menghubungi kami melalui informasi yang sudah tertera pada bagian contact kami.</p>
+		</div>
+	</div>
+	<div class="row">
+
+		<div class="col-xs-offset-1 col-xs-5 box-color-2">
+			<hr class="veryShortRedHR"/>
+			<h1 class="text-center text-uppercase specialBlackColor">Lunasi Pembayaran</h1>
+			<p>Kami menerima pembayaran dengan menggunakan Bank BCA. Silahkan menghubungi kami untuk menanyakan nomor rekening.</p>
+		</div>
+
+		<div class="col-xs-5 clearfix box-color-1">
+			<hr class="veryShortRedHR"/>
+			<h1 class="text-center text-uppercase specialBlackColor">Pengiriman Barang</h1>
+			<p>Agar kami dapat melakukan pengiriman barang, segera bayar pesanan anda termasuk dengan ongkir ditanggung oleh customer. Semua pengiriman kami lakukan dengan JNE</p>
+		</div>
+	</div>
+</div>
+<!-- END of "How to order" -->
+
+<!-- Contact -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<h1 class="specialRedColor text-uppercase">
+				Contact
+			</h1>
+
+		</div>
+	</div><!-- END of .row -->
+
+	<div class="row">
+		<div class="col-xs-4 text-right specialBlackColor"><p><img src="<?php echo get_template_directory_uri(); ?>/asset/images/whatsapp.png" alt=""/>+62 812 892 205 22</p></div>
+		<div class="col-xs-4 text-center specialBlackColor"><p><img src="<?php echo get_template_directory_uri(); ?>/asset/images/blackberry.png" alt=""/>7D0F8F24</p></div>
+		<div class="col-xs-4 text-left specialBlackColor"><p><img src="<?php echo get_template_directory_uri(); ?>/asset/images/line.png" height="50" alt=""/>INDO.BALON</p></div>
+	</div><!-- END of .row -->
+
+	<div class="row">
+		<form role="form">
+
+			<div class="col-xs-6">
+
+				<div class="form-group">
+					<label for="name">Name</label>
+					<input type="name" class="form-control" id="name" name="name" placeholder="Enter Name">
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="Enter Email">
+				</div>
+				<div class="form-group">
+					<label for="phone">Phone</label>
+					<input type="tel" class="form-control" id="phone" name="phone" placeholder="Enter Phone Number">
+				</div>
+
+			</div>
+
+			<div class="col-xs-6">
+				<div class="form-group">
+					<label for="comment">Comment</label>
+					<textarea cols="50" rows="5" placeholder="Give us your comment..." name="comment" id="comment" class="form-control"></textarea>
+				</div>
+				<button type="submit" class="btn btn-default pull-right">Submit</button>
+			</div>
+
+		</form>
+	</div><!-- END of .row -->
+
+	<div class="row">
+		<div class="col-xs-12">
+			<hr class="shortRedHR" />
+			<h1 class="text-center text-uppercase specialBlackColor">Connect Socially With <br />Indobalon</h1>
+
+		</div>
+		<div class="col-xs-4 text-right">
+			<span class="fa-stack fa-lg fa-2x specialRedColor">
+			    <i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+			</span>
+		</div>
+		<div class="col-xs-4 text-center">
+			<span class="fa-stack fa-lg fa-2x specialRedColor">
+			    <i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+			</span>
+		</div>
+		<div class="col-xs-4">
+			<span class="fa-stack fa-lg fa-2x specialRedColor">
+			    <i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+			</span>
+		</div>
+	</div><!-- END of .row -->
+
+	<div class="row">
+		<div class="col-xs-12 specialRedBackgroundColor">
+			<p class="specialPinkColor text-center">&copy; 2014 indobalon. All rights reserved indobalon@gmail.com Made by vitajaya &reg; </p>
+		</div>
+	</div><!-- END of .row -->
+
+</div>
+<!-- END of Contact -->
+
+</body>
+</html>
+
